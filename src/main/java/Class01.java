7bc9cf99899bbc802bc7cfaaada189905d2d7521
esracnykn00 <esrakaraoglan00@gmail.com>
2023-05-06 22:17:16 +0300

    v2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -3,11 +3,7 @@ public class Class01 {
     public static void main(String[] args) {
 
         //V1 Calismalarim...
-        //V2 uzerine calismalar...
-        System.out.println("v3 kodlari...");
-        //V4 kisayoldan commit
-        //V5 kisa yoldan commit
-
+        //V2 kodlari...
 
 
 
